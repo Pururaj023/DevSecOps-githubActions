0b85d77504f29b2be1a084b5aee8fb924306bde7
Pururaj069 <PururajChoudhary69@gmail.com>
2025-09-29 19:30:57 +0530

    added terrafrom and CD workflow

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_security_group" "sg" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y docker
+              systemctl enable docker
+              systemctl start docker
+              usermod -aG docker ec2-user
+              EOF
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
---
+++ terraform/variable.tf
@@ -0,0 +1,17 @@
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "ami_id" {
+    default = "ami-0953476d60561c955"
+  
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "key_name" {
+    default = "testkey"
+}
+
